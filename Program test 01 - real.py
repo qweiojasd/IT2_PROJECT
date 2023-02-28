45aac0c65df954ed073e6235c7d25735fd53537f
qweiojasd <hsuweilem@gmail.com>
2023-02-28 12:07:59 +0100

    Hello

--- Program test 01 - real.py
+++ Program test 01 - real.py
@@ -11,12 +11,13 @@ def main():
 class GraphMaker(Frame):
     def __init__(self):
         tk.Frame.__init__(self)
-        self.pack()
+        self.pack( side = TOP )
         self.master.title("Window")
         self.button1 = Button(self,)
         self.quit_button = Button(self, text="Quit" )
         self.quit_button.grid(row=2, column=0)
         self.quit_button['command'] = self.close_window
+        self.place(bordermode=OUTSIDE, height=100, width=100)
 
     def close_window(self):
         self.destroy()
